cf09300254e971d42a472df2aa99127d8566dc81
raxkumar-wedaa <rakesh.c@wedaa.tech>
2024-04-27 10:38:00 +0530

    Initial commit: Generated fastapi blueprint

---
+++ generator-jhipster-fastapi/generators/server/generator.mjs
@@ -0,0 +1,165 @@
+import chalk from "chalk";
+import ServerGenerator from "generator-jhipster/generators/server";
+
+export default class extends ServerGenerator {
+  constructor(args, opts, features) {
+    super(args, opts, features);
+
+    if (this.options.help) return;
+
+    if (!this.options.jhipsterContext) {
+      throw new Error(
+        `This is a JHipster blueprint and should be used only like ${chalk.yellow(
+          "jhipster --blueprints fastapi"
+        )}`
+      );
+    }
+  }
+
+  get [ServerGenerator.INITIALIZING]() {
+    return {
+      ...super.initializing,
+      async initializingTemplateTask() {},
+    };
+  }
+
+  get [ServerGenerator.PROMPTING]() {
+    return {
+      ...super.prompting,
+      async promptingTemplateTask() {},
+    };
+  }
+
+  get [ServerGenerator.CONFIGURING]() {
+    return {
+      ...super.configuring,
+      async configuringTemplateTask() {},
+    };
+  }
+
+  get [ServerGenerator.COMPOSING]() {
+    return {
+      ...super.composing,
+      async composingTemplateTask() {},
+    };
+  }
+
+  get [ServerGenerator.LOADING]() {
+    return {
+      ...super.loading,
+      async loadingTemplateTask() {},
+    };
+  }
+
+  get [ServerGenerator.PREPARING]() {
+    return {
+      ...super.preparing,
+      async preparingTemplateTask() {},
+    };
+  }
+
+  get [ServerGenerator.CONFIGURING_EACH_ENTITY]() {
+    return {
+      ...super.configuringEachEntity,
+      async configuringEachEntityTemplateTask() {},
+    };
+  }
+
+  get [ServerGenerator.LOADING_ENTITIES]() {
+    return {
+      ...super.loadingEntities,
+      async loadingEntitiesTemplateTask() {},
+    };
+  }
+
+  get [ServerGenerator.PREPARING_EACH_ENTITY]() {
+    return {
+      ...super.preparingEachEntity,
+      async preparingEachEntityTemplateTask() {},
+    };
+  }
+
+  get [ServerGenerator.PREPARING_EACH_ENTITY_FIELD]() {
+    return {
+      ...super.preparingEachEntityField,
+      async preparingEachEntityFieldTemplateTask() {},
+    };
+  }
+
+  get [ServerGenerator.PREPARING_EACH_ENTITY_RELATIONSHIP]() {
+    return {
+      ...super.preparingEachEntityRelationship,
+      async preparingEachEntityRelationshipTemplateTask() {},
+    };
+  }
+
+  get [ServerGenerator.POST_PREPARING_EACH_ENTITY]() {
+    return {
+      ...super.postPreparingEachEntity,
+      async postPreparingEachEntityTemplateTask() {},
+    };
+  }
+
+  get [ServerGenerator.DEFAULT]() {
+    return {
+      ...super.default,
+      async defaultTemplateTask() {},
+    };
+  }
+
+  get [ServerGenerator.WRITING]() {
+    return {
+      ...super.writing,
+      async writingTemplateTask() {
+        await this.writeFiles({
+          sections: {
+            files: [{ templates: ["template-file-server"] }],
+          },
+          context: this,
+        });
+      },
+    };
+  }
+
+  get [ServerGenerator.WRITING_ENTITIES]() {
+    return {
+      ...super.writingEntities,
+      async writingEntitiesTemplateTask() {},
+    };
+  }
+
+  get [ServerGenerator.POST_WRITING]() {
+    return {
+      ...super.postWriting,
+      async postWritingTemplateTask() {},
+    };
+  }
+
+  get [ServerGenerator.POST_WRITING_ENTITIES]() {
+    return {
+      ...super.postWritingEntities,
+      async postWritingEntitiesTemplateTask() {},
+    };
+  }
+
+  get [ServerGenerator.INSTALL]() {
+    return {
+      ...super.install,
+      async installTemplateTask() {},
+    };
+  }
+
+  get [ServerGenerator.POST_INSTALL]() {
+    return {
+      ...super.postInstall,
+      async postInstallTemplateTask() {},
+    };
+  }
+
+  get [ServerGenerator.END]() {
+    return {
+      ...super.end,
+      async endTemplateTask() {},
+    };
+  }
+}
---
+++ generator-jhipster-fastapi/test/utils.mjs
@@ -0,0 +1,19 @@
+import { createHelpers } from "yeoman-test";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+// eslint-disable-next-line import/no-unresolved
+import { EnvironmentBuilder } from "generator-jhipster/cli";
+
+const packageRoot = join(dirname(fileURLToPath(import.meta.url)), "..");
+const jhipsterPath = join(packageRoot, "node_modules", "generator-jhipster");
+
+export function createTestHelpers(options = {}) {
+  return createHelpers({ ...options, createEnv: EnvironmentBuilder.createEnv });
+}
+
+export const helpers = createTestHelpers();
+
+export const lookups = [
+  { packagePaths: [jhipsterPath], lookups: ["dist/generators"] },
+  { packagePaths: [packageRoot], lookups: ["generators"] },
+];
